b75a09f75c2bb070bc0dd071a83ba047b2b8f747
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2021-01-28 20:23:35 +0100

    cw4

---
+++ cw4/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace cw3.Controllers{
+
+    [ApiController]
+    [Route("api/students")]
+
+      public class StudentsController : ControllerBase{
+
+        private const string Conncetion = "Data Source=db-mssql;Initial Catalog=s189823;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudent() {
+            List<Student> studentsList = new List<Student>();
+            Student student;
+            using (SqlConnection conn = new SqlConnection(Conncetion))
+            using (SqlCommand command = new SqlCommand()) {
+                command.Connection = conn;
+                command.CommandText = "select * from student";
+
+                conn.Open();
+                SqlDataReader sqlReader = command.ExecuteReader();
+                while (sqlReader.Read()) {
+                    student = new Student();
+                    student.IndexNumber = sqlReader["IndexNumber"].ToString();
+                    student.FirstName = sqlReader["FirstName"].ToString();
+                    student.LastName = sqlReader["LastName"].ToString();
+                    student.Birthdate = sqlReader["BirthDate"].ToString();
+                    student.IdEnrollment = Int32.Parse(sqlReader["IdEnrollment"].ToString());
+                    studentsList.Add(student);
+                }
+                sqlReader.Close();
+            }
+            return Ok(studentsList);
+        }
+
+        [HttpGet("{IndexNumber}")]
+        public IActionResult GetStudent(string IndexNumber) {
+            Student student;
+            Enrollment enrollment = new Enrollment();
+            Studies studies = new Studies();
+            List<Object> query = new List<object>();
+            using (SqlConnection conn = new SqlConnection(Conncetion))
+            using (SqlCommand command = new SqlCommand()){
+                command.Connection = conn;
+                command.CommandText = "select Student.idEnrollment, semester, name, startdate from Enrollment inner join student on Enrollment.IdEnrollment = Student.IdEnrollment inner join Studies on Enrollment.idStudy = Studies.idStudy where Student.IndexNumber = @index";
+
+                command.Parameters.AddWithValue("index", IndexNumber);
+
+                conn.Open();
+                var sqlReader = command.ExecuteReader();
+                if(sqlReader.Read()){
+                    enrollment.IdEnrollment = Int32.Parse(sqlReader["IdEnrollment"].ToString());
+                    enrollment.Semester = Int32.Parse(sqlReader["Semester"].ToString());
+                    studies.Name = sqlReader["Name"].ToString();
+                    enrollment.Startdate = sqlReader["StartDate"].ToString();
+
+                    query.Add(enrollment.IdEnrollment);
+                    query.Add(enrollment.Semester);
+                    query.Add(studies.Name);
+                    query.Add(enrollment.Startdate);
+                    return Ok(query);
+                }
+                sqlReader.Close();
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ cw4/cw3/DAL/MockDbService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.DAL{
+    public class MockDbService : IDbService{
+        private static IEnumerable<Student> studentsList;
+
+        static MockDbService(){
+            studentsList = new List<Student>()
+            {
+                new Student{ idNumber = 1, firstName = "Cezary  ",lastName = "Boguszewski", indexNumber = "s1"},
+                new Student{ idNumber = 2, firstName = "Cezary  ",lastName = "Boguszewski", indexNumber = "s2"},
+                new Student{ idNumber = 3, firstName = "Cezary  ",lastName = "Boguszewski", indexNumber = "s3"},
+                
+            };
+
+        }
+        public IEnumerable<Student> GetStudents(){
+            return studentsList;
+            throw new NotImplementedException();
+        }
+    }
+}
