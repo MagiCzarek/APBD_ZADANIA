ade3572bcb1b7ff8a4bd3e1632a1473e2524625a
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2021-01-28 18:32:44 +0100

    zad3
    zad3

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace cw3.Controllers{
+
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase{
+        private IDbService dbService;
+
+        public StudentsController(IDbService dbService) {
+            this.dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy) {
+            if (orderBy == "indexNumber") {
+                return Ok(dbService.GetStudents().OrderBy(idx => idx.indexNumber));
+            }
+            return Ok(dbService.GetStudents());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id) {
+            if (id == 1) {
+                return Ok("Boguszewski");
+            } else if (id == 2) {
+                return Ok("XD");
+            }
+            else{
+                return NotFound("Nie znaleziono studenta");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student){
+            student.indexNumber = $"s{new Random().Next(1,20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id) {
+            return Ok($"Aktualizacja studenta o id={id}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id) {
+            return Ok($"Usuwanie studenta o id={id}");
+        }
+    }
+}
---
+++ cw3/cw3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3
+{
+    public class Student{
+        public int idNumber { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string indexNumber { get; set; }
+    }
+}
