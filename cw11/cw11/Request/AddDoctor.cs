96836f71c9592049ddcfbef7d2ba2a7bc39f7b13
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2021-01-29 17:54:47 +0100

    finally 11 YUMMY
    cw11

---
+++ cw11/cw11/Request/AddDoctor.cs
@@ -0,0 +1,21 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Request
+{
+    public class AddDoctor
+    {
+        [Required]
+        public int IdDoctor { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Email]
+        public string Email { get; set; }
+    }
+}
